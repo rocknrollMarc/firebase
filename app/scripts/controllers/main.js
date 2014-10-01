999ebcb05701188c0565d957bc7e06da09bd59d1
Marc <marc.bluemner@gmail.com>
2014-10-01 06:49:32 +0200

    update value

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,6 +13,16 @@ angular.module('firebaseApp')
     var rootRef = new Firebase('https://shining-torch-2720.firebaseio.com/');
     var childRef = rootRef.child('message');
 
+    childRef.on('value', function(snapshot) {
+      var snapshotVal =  snapshot.val();
+      console.log(snapshotVal);
+      $scope.message = snapshot.val();
+    });
+
+    $scope.$watch('message.text', function(newVal) {
+      console.log(newVal);
+    });
+
     // https://shining-torch-2720.firebaseio.com/message
     $scope.setMessage = function() {
       rootRef.child('message').set({
