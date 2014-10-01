e4a83fe238b238e06309377338e170841ed65a50
Marc <marc.bluemner@gmail.com>
2014-10-01 06:06:01 +0200

    yeoman setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firebaseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firebaseApp
+ */
+angular.module('firebaseApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
