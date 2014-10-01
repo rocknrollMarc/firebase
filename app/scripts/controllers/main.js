59a569313bc342c9cfe008c0294b299e7e47fe9f
Marc <marc.bluemner@gmail.com>
2014-10-01 06:14:35 +0200

    added firebase instance in main controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('firebaseApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    var rootRef = new Firebase('https://shining-torch-2720.firebaseio.com/')
   });
